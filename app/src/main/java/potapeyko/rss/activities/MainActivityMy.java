80b16747a8e1d77fc75964decb7911b12d236767
Potapeyko_Dmitry <potapeiko.dmity@yandex.ru>
2016-10-29 23:21:19 +0700

    Added pre style of MainActivity

--- app/src/main/java/potapeyko/rss/activities/MainActivityMy.java
+++ app/src/main/java/potapeyko/rss/activities/MainActivityMy.java
@@ -7,15 +7,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import potapeyko.rss.R;
+import potapeyko.rss.adapters.ItemsListAdapter;
 import potapeyko.rss.interfaces.IActivityListener;
+import potapeyko.rss.models.News;
+
+import java.util.ArrayList;
 
 
 public class MainActivityMy extends MyBaseActivity implements IActivityListener {
-    private TextView text;
-
     private String[] mDrawerTitles;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
+
+    private TextView text;
+    private ListView mNewsList;
 
     public MainActivityMy() {
         this.onCreateSubscribe(this);
@@ -27,13 +32,49 @@ public class MainActivityMy extends MyBaseActivity implements IActivityListener 
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.textView);
         text.setText("Измененный текст");
+        leftDrawerLayoutInit();
+        newsListInit();
+    }
 
+
+    private void leftDrawerLayoutInit() {
         mDrawerTitles = getResources().getStringArray(R.array.drawer_items_array);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 R.layout.drawer_list_item, mDrawerTitles));
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener(mDrawerLayout, mDrawerList,this));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener(mDrawerLayout, mDrawerList, this));
     }
+    private void  newsListInit(){
+        mNewsList = (ListView) findViewById(R.id.newsList);
+        ItemsListAdapter adapter = new ItemsListAdapter(this, getItemsList());
+        mNewsList.setAdapter(adapter);
+    }
+    //todo времянка замена на базу
+    private ArrayList<News> getItemsList() {
+
+
+        ArrayList<News> news = new ArrayList<>();
+        news.add(new News("Новость 1", "Описание новости Описание новости Описание новости Описание новости Описание новости",
+                "http://ru.stackoverflow.com/questions/417548/%D0%A0%D0%B0%D0%B7%D0%BC%D0%B5%D1%80-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9-%D0%BD%D0%B0-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B5-android-studio")) ;
+        news.add(new News("Новость 1", "Описание новости Описание новости Описание новости Описание новости Описание новости",
+                "http://ru.stackoverflow.com/questions/417548/%D0%A0%D0%B0%D0%B7%D0%BC%D0%B5%D1%80-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9-%D0%BD%D0%B0-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B5-android-studio"));
+        news.add(new News("Новость 2", "Описание новости Описание новости Описание новости Описание новости Описание новости",
+                "http://ru.stackoverflow.com/questions/417548/%D0%A0%D0%B0%D0%B7%D0%BC%D0%B5%D1%80-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9-%D0%BD%D0%B0-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B5-android-studio"
+                ));
+        news.add(new News("Новость3 ", "Описание новости Описание новости Описание новости Описание новости Описание новости",
+                "http://ru.stackoverflow.com/questions/417548/%D0%A0%D0%B0%D0%B7%D0%BC%D0%B5%D1%80-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9-%D0%BD%D0%B0-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B5-android-studio"
+                ));
+        news.add(new News("Новость 4", "Описание новости Описание новости Описание новости Описание новости Описание новости",
+                "http://ru.stackoverflow.com/questions/417548/%D0%A0%D0%B0%D0%B7%D0%BC%D0%B5%D1%80-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9-%D0%BD%D0%B0-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B5-android-studio"
+                ));
+        news.add(new News("Новость 5", "Описание новости Описание новости Описание новости Описание новости Описание новости",
+                "http://ru.stackoverflow.com/questions/417548/%D0%A0%D0%B0%D0%B7%D0%BC%D0%B5%D1%80-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9-%D0%BD%D0%B0-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B5-android-studio"
+                ));
+        news.add(new News("Новость 6", "Описание новости Описание новости Описание новости Описание новости Описание новости",
+                "http://ru.stackoverflow.com/questions/417548/%D0%A0%D0%B0%D0%B7%D0%BC%D0%B5%D1%80-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9-%D0%BD%D0%B0-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B5-android-studio"
+                ));
+        return news;
+    }
+
 }
--- app/src/main/java/potapeyko/rss/models/News.java
+++ app/src/main/java/potapeyko/rss/models/News.java
@@ -1,14 +1,21 @@
 package potapeyko.rss.models;
 
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@Data
 public class News {
-    @Getter    @Setter    String title;
-    @Getter    @Setter    String description;
-    @Getter    @Setter    String fullNewsUri;
+    String title;
+    String description;
+    String fullNewsUri;
 
+    public News(String title, String description, String fullNewsUri){
+        this.title=title;
+        this.description=description;
+        this.fullNewsUri=fullNewsUri;
+    }
 
 
 //    @Getter    @Setter    int imageResource;
