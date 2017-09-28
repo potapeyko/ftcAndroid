5da3e0f38db6cf81f1326e998173361495b2f469
Potapeyko_Dmitry <dmitry.potapeiko@yandex.ru>
2017-09-29 01:20:27 +0700

    Добавил асинктаск и его сохранение при перезапуске активности Асинктаск не готов

--- app/src/main/java/potapeyko/rss/activities/NewFeedActivity.java
+++ app/src/main/java/potapeyko/rss/activities/NewFeedActivity.java
@@ -5,30 +5,127 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.util.Patterns;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.*;
 import lombok.NonNull;
 import potapeyko.rss.R;
+import potapeyko.rss.model.Feed;
 import potapeyko.rss.network.NetworkHelper;
 import potapeyko.rss.utils.BroadcastSender;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static potapeyko.rss.utils.BroadcastSender.*;
 
-
+//todo: не влазит на экран при горизонтальной ориентации экрана. + сделать поход в интернет.
 public final class NewFeedActivity extends MyBaseActivity {
 
-    private Button btnNewChanel;
+    private Button btnConnectNewChanel;
+    private Button btnFindNewChanel;
     private EditText etUri;
+    private EditText etKeywords;
+    private ListView lvFeedsList;
     private TextView tvNewChanel;
     private Spinner spUrlProtocol;
     private final NetworkHelper nwHelper;
-    private ProgressBar progressBar;
+    private LinearLayout progressBarLayout; //progressBarLayout inside this linearLayout
 //    int o=0;
+
+    private GetListOfFeedsAsynkTask getListOfFeedsAsynkTask;
+
+    /// сохранение асинктаска при повороте экрана.
+    @Override
+    public Object onRetainCustomNonConfigurationInstance() {
+
+        if (getListOfFeedsAsynkTask != null)
+            getListOfFeedsAsynkTask.unlink();
+        return getListOfFeedsAsynkTask;
+
+    }
+
+    static private class GetListOfFeedsAsynkTask extends AsyncTask<String, Void, Void> {
+        NewFeedActivity newFeedActivity;
+
+        public ArrayList<Feed> getFeeds() {
+            return feeds;
+        }
+
+        ArrayList<Feed> feeds;
+
+        public final void link(NewFeedActivity activity) {
+            newFeedActivity = activity;
+        }
+
+        public final void unlink() {
+            newFeedActivity = null;
+        }
+
+        @Override
+        protected Void doInBackground(String... url) {
+            //сходить на сайт, распарсить, отдать
+            try {
+                Thread.sleep(5000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (newFeedActivity != null) {
+                newFeedActivity.createFeedsList(feeds);
+            }
+        }
+    }
+
+
+    private void createFeedsList(ArrayList<Feed> s) {
+        if (lvFeedsList != null) {
+            Feed[] a = {new Feed(1, "World of Tanks", "https://worldoftanks.ru/ru/rss/news/", "link1", "Des",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0), new Feed(2, "title2", "link2", "link1", "Des2",
+                    null, null, 0)};
+            s = new ArrayList<Feed>(Arrays.asList(a));
+            ArrayAdapter arrayAdapter = createArrayAdapter(s);
+            lvFeedsList.setAdapter(arrayAdapter);
+            lvFeedsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    TextView urlView = (TextView) view.findViewById(R.id.newfeeds_list_item_url);
+                    if (urlView != null) {
+                        AddChannelIntentService.startActionADD(getApplicationContext(), urlView.getText().toString());
+                        progressBarLayout.setVisibility(ProgressBar.VISIBLE);
+                    }
+                }
+            });
+            //http://cloud.feedly.com/v3/search/feeds?count=50&q=sport
+        }
+        progressBarLayout.setVisibility(ProgressBar.INVISIBLE);//убираем прогресс
+        activateControls();
+    }
 
     final private BroadcastReceiver br = new BroadcastReceiver() {
         @Override
@@ -47,9 +144,8 @@ public final class NewFeedActivity extends MyBaseActivity {
             } else if (CHANNEL_ADD_BROADCAST_MESS.equals(broadcastMessage)) {
                 showCauseToast(broadcastMessage);
             } else {//какая то ошибка
-                btnNewChanel.setEnabled(true);
-                etUri.setEnabled(true);
-                progressBar.setVisibility(ProgressBar.INVISIBLE);//убираем прогресс
+                NewFeedActivity.this.activateControls();
+                progressBarLayout.setVisibility(ProgressBar.INVISIBLE);//убираем прогресс
                 showCauseToast(broadcastMessage); // сообщения о причинах ошибки
             }
 
@@ -72,10 +168,26 @@ public final class NewFeedActivity extends MyBaseActivity {
 
     public NewFeedActivity() {
         nwHelper = new NetworkHelper(this);
+    }//а че он не Singleton?
+
+    private void deactivateControls() {
+        btnConnectNewChanel.setEnabled(false);
+        btnFindNewChanel.setEnabled(false);
+        etUri.setEnabled(false);
+        etKeywords.setEnabled(false);
+        spUrlProtocol.setEnabled(false);
+    }
+
+    private void activateControls() {
+        btnConnectNewChanel.setEnabled(true);
+        btnFindNewChanel.setEnabled(true);
+        etUri.setEnabled(true);
+        etKeywords.setEnabled(true);
+        spUrlProtocol.setEnabled(true);
     }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LocalBroadcastManager.getInstance(NewFeedActivity.this).registerReceiver(br, new IntentFilter(BroadcastSender.INTENT_FILTER));
         setContentView(R.layout.activity_new_feed);
@@ -87,19 +199,21 @@ public final class NewFeedActivity extends MyBaseActivity {
                 finish();
             }
         });
-        btnNewChanel = (Button) findViewById(R.id.activity_new_feed_btnNewChanel);
+        btnConnectNewChanel = (Button) findViewById(R.id.activity_new_feed_btnConnectNewChanel);
+        btnFindNewChanel = (Button) findViewById(R.id.activity_new_feed_btnFindNewChanel);
         etUri = (EditText) findViewById(R.id.activity_new_feed_etNewChanelUri);
+        etKeywords = (EditText) findViewById(R.id.activity_new_feed_etFindNewChanelKeywords);
 
 
         tvNewChanel = (TextView) findViewById(R.id.activity_new_feed_tvNewChanel);
         spUrlProtocol = (Spinner) findViewById(R.id.activity_new_feed_spUrlProtocol);
+        lvFeedsList = (ListView) findViewById(R.id.activity_new_feed_list);
+        progressBarLayout = (LinearLayout) findViewById(R.id.activity_new_feed_progressBar_layout);
 
-
-        progressBar = (ProgressBar) findViewById(R.id.activity_new_feed_progressBar);
         if (!nwHelper.isNetworkAvailable()) {
             notConnectionCase();
         } else {
-            btnNewChanel.setOnClickListener(new View.OnClickListener() {
+            btnConnectNewChanel.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     String uri = getUri();
@@ -109,37 +223,79 @@ public final class NewFeedActivity extends MyBaseActivity {
                                 Toast.LENGTH_SHORT).show();
                         return;
                     }
-
                     AddChannelIntentService.startActionADD(getApplicationContext(), uri);
-                    btnNewChanel.setEnabled(false);
-                    etUri.setEnabled(false);
-                    progressBar.setVisibility(ProgressBar.VISIBLE);
-
-//                    spUrlProtocol.setSelection(o,false);
-//                    o=++o%2;
+                    deactivateControls();
+                    progressBarLayout.setVisibility(ProgressBar.VISIBLE);
                 }
             });
 
+            btnFindNewChanel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    deactivateControls();
+                    progressBarLayout.setVisibility(ProgressBar.VISIBLE);
+                    getListOfFeedsAsynkTask = new GetListOfFeedsAsynkTask();
+                    getListOfFeedsAsynkTask.link(NewFeedActivity.this);
+                    getListOfFeedsAsynkTask.
+                            execute("http://cloud.feedly.com/v3/search/feeds?count=50&q=sport");
+                }
+            });
+            btnFindNewChanel.requestFocus();
         }
+
+
+        //получаем сохраненную при пересоздании активности задачу
+        getListOfFeedsAsynkTask = (GetListOfFeedsAsynkTask) getLastCustomNonConfigurationInstance();
+        if (getListOfFeedsAsynkTask != null) {
+            if (getListOfFeedsAsynkTask.getStatus() != AsyncTask.Status.FINISHED) {
+                getListOfFeedsAsynkTask.link(this);
+                progressBarLayout.setVisibility(ProgressBar.VISIBLE);
+                getListOfFeedsAsynkTask = new GetListOfFeedsAsynkTask();
+            } else {
+                createFeedsList(getListOfFeedsAsynkTask.getFeeds());
+            }
+        }
+    }
+
+    private ArrayAdapter createArrayAdapter(final ArrayList a) {
+        return new ArrayAdapter<Feed>(getApplicationContext(),
+                R.layout.newfeeds_list_item, a) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                Feed feed = getItem(position);
+                if (convertView == null) {
+                    convertView = LayoutInflater.from(getContext())
+                            .inflate(R.layout.newfeeds_list_item, null);
+                }
+                ((TextView) convertView.findViewById(R.id.newfeeds_list_item_title))
+                        .setText(feed.getTitle());
+                ((TextView) convertView.findViewById(R.id.newfeeds_list_item_url))
+                        .setText(feed.getLink());
+                ((TextView) convertView.findViewById(R.id.newfeeds_list_item_description))
+                        .setText(feed.getDescription());
+                return convertView;
+            }
+        };
     }
 
     private String getUri() {
         String uri = etUri.getText().toString();
         String http_https[] = getResources().getStringArray(R.array.http_https);
-        if (!uri.contains("://")) {
+        if (!uri.contains("://")) {//no protocol in url => берем из spinner
             return getResources().getStringArray(R.array.http_https)[spUrlProtocol.getSelectedItemPosition()] + uri;
         } else {
             String[] s = uri.split("://", 2);
-            etUri.setText(s[1]);
+
             String protocol = s[0] + "://";
             for (int i = 0; i < http_https.length; i++) {
 
                 if (http_https[i].equals(protocol)) {
                     spUrlProtocol.setSelection(i, false);
+                    etUri.setText(s[1]);
                     break;
                 }
             }
-            return uri;
+            return uri;//есть :// но не протокол. обработается при попытке обращения
         }
     }
 
@@ -148,8 +304,8 @@ public final class NewFeedActivity extends MyBaseActivity {
     }
 
     private void notConnectionCase() {
-        btnNewChanel.setText(R.string.back);
-        btnNewChanel.setOnClickListener(new View.OnClickListener() {
+        btnConnectNewChanel.setText(R.string.back);
+        btnConnectNewChanel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
